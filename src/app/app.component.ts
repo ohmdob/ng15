632d0e7b59ae1ee1e0dc71dca6ee29e9ed18cbbc
ohmdob <ohm10513@gmail.com>
2023-04-07 16:21:42 +0700

    init for wawza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { WowzaWebRTCPlayer } from "wowza-webrtc-player";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng15';
+  async ngAfterViewInit() {
+    const videoElement = document.querySelector("video");
+    const player = new WowzaWebRTCPlayer(videoElement as HTMLVideoElement, {
+      sdpUrl: "wss://zeezzrezrezr.streamlock.net/webrtc-session.json",
+      applicationName: "webrtc",
+      streamName: "myStream"
+    });
+    await player.playRemote();
+  }
 }
